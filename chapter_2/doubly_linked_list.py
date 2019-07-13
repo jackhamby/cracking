c8338c985a7b79d8b8f0ade24e50e8973cbd4a47
JackHamby <jackhamby@JackHambys-MacBook-Pro.local>
2019-07-13 18:51:50 -0500

    added remove method

--- chapter_2/doubly_linked_list.py
+++ chapter_2/doubly_linked_list.py
@@ -6,6 +6,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
     def __repr__(self):
         if (not self.head):
@@ -25,6 +26,7 @@ class LinkedList:
         return f'[{output}]'
 
     def insert(self, value):
+        self.length += 1
         if (not self.head):
             self.head = Node(value)
             self.tail = self.head
@@ -37,7 +39,26 @@ class LinkedList:
                 return self.head
             head = head.next
 
-        
+    def remove(self, value):
+        if (not self.head):
+            return self.head
+        if (self.head.value == value):
+            self.head = self.head.next
+            self.head.prev = None
+            return self.head
+        head = self.head
+        nxt = head.next
+        while(nxt):
+            if (nxt.value == value):
+                head.next = nxt.next
+                if (not head.next):
+                    self.tail = head
+                    return
+                head.next.prev = head
+                return
+            head = nxt
+            nxt = head.next
+
 
 
 class Node:
@@ -54,6 +75,10 @@ lst.insert(2)
 lst.insert(4)
 lst.insert(5)
 lst.insert(7)
+lst.remove(7)
+lst.insert(4)
+
+
 print(lst)
 print(f'head value {lst.head.value}')
 print(f'tail value {lst.tail.value}')
