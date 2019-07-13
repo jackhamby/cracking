35e35e5bed862d4bd2aaae1616944367c478ce0d
JackHamby <jackhamby@JackHambys-MacBook-Pro.local>
2019-07-13 14:52:30 -0500

    LL

---
+++ chapter_2/linked_list.py
@@ -0,0 +1,78 @@
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+
+    def __repr__(self):
+        head = self.head
+        string = "["
+        while(head):
+            if (not head.next):
+                string += f'{head.value}'
+            else:
+                string += f'{head.value},'
+            head = head.next
+        string += "]"
+        return string
+
+    def insert(self, value):
+        if (not self.head):
+            self.head = Node(value)
+            return self.head
+        head = self.head
+        while(head):
+            if (not head.next):
+                head.next = Node(value)
+                return self.head
+            head = head.next
+
+        
+    def remove(self, value):
+        if (not self.head):
+            return None
+
+        if (self.head.value == value):
+            self.head = self.head.next
+            return self.head
+
+        head = self.head 
+        nxt = self.head.next            
+        while(nxt):
+            if (nxt.value == value):
+                head.next = nxt.next
+                return self.head
+                # Need to remove
+            head = nxt
+            nxt = nxt.next
+
+
+
+class Node:
+
+
+    def __init__(self, value):
+        self.next = None
+        self.value = value
+
+
+
+
+
+
+lst = LinkedList()
+lst.insert(5)
+lst.insert(2)
+lst.insert(10)
+lst.remove(10)
+lst.remove(5)
+lst.remove(2)
+print(lst)
+
+
+
+
+
+
